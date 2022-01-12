3f0aa6fb98cdfddd227375f06d28391d44844283
KONATE Bakary <b0konate@enib.fr>
2022-01-13 00:39:39 +0100

    first commit

---
+++ src/components/TotalBudgetCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useBudgets } from '../contexts/BudgetsContext'
+import BudgetCard from './BudgetCard'
+
+function TotalBudgetCard() {
+
+    const {expenses, budgets } = useBudgets();
+    const amount =  expenses.reduce((total,expense) => total + expense.amount, 0)
+    const max =  budgets.reduce((total,budget) => total + budget.max, 0)
+     if (amount === 0) return null
+    return (
+        <BudgetCard amount={amount} name="Total" gray max={max}  hideButtons />
+    ) 
+}
+
+export default TotalBudgetCard
